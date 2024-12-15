32400a1b624844e7e24fcebec55a012a46043ff8
Lucas Neuhaus <lucas.neuhaus@bairesdev.com>
2024-12-15 12:50:42 -0300

    Updating the load backup action

--- apps/sveltekit/src/routes/backup/+page.server.ts
+++ apps/sveltekit/src/routes/backup/+page.server.ts
@@ -1,27 +1,41 @@
-import { loadBackup } from "$lib/server/data/backup";
+import { loadBackup } from "$lib/server/data/backup.js";
 import { USER_ADMIN_ID } from "$lib/user.js";
 import { error, fail, redirect } from "@sveltejs/kit";
 
 export const load = async (event) => {
-  if (!event.locals.user) {
-    return redirect(302, "/login");
-  }
+	if (!event.locals.user) {
+		return redirect(302, "/login");
+	}
 
-  const userId = event.locals.user.id;
-  if (userId !== USER_ADMIN_ID) {
-    return fail(404);
-  }
+	const userId = event.locals.user.id;
+	if (userId !== USER_ADMIN_ID) {
+		return fail(404);
+	}
 
-  return {};
+	return {};
 };
 
 export const actions = {
-  "load-backup": async (event) => {
-    const user = event.locals.user;
-    if (!user || user.id !== USER_ADMIN_ID) {
-      return error(401);
-    }
+	"load-backup": async (event) => {
+		const user = event.locals.user;
+		console.log("[1]", user);
+		if (!user || user.id !== USER_ADMIN_ID) {
+			return error(401);
+		}
 
-    return loadBackup(user.id);
-  },
-}
+		const formData = await event.request.formData();
+		const file = formData.get("file");
+
+		if (!(file instanceof File) || file.type !== "application/json") {
+			return error(400, "Invalid file type");
+		}
+
+		const fileContent = await file.text();
+		const jsonData = JSON.parse(fileContent);
+
+		// Process the JSON data as needed
+		console.log(jsonData);
+
+		return loadBackup(user.id, jsonData);
+	},
+};
--- apps/sveltekit/src/routes/backup/+page.svelte
+++ apps/sveltekit/src/routes/backup/+page.svelte
@@ -22,13 +22,19 @@
 			<Card.Title>Load Backup</Card.Title>
 		</Card.Header>
 		<Card.Content>
-			<form method="post" action="?/load-backup" class="">
+			<form method="post" action="?/load-backup" class="" enctype="multipart/form-data">
 				<div class="mb-3 flex flex-col gap-2">
 					<Label for="backup-json">Backup File</Label>
-					<Input id="backup-json" type="file" class="w-full"></Input>
+					<Input
+						id="backup-json"
+						name="file"
+						type="file"
+						accept="application/json"
+						class="w-full"
+					></Input>
 				</div>
 
-				<Button>Load Backup</Button>
+				<Button type="submit">Load Backup</Button>
 			</form>
 		</Card.Content>
 	</Card.Root>
