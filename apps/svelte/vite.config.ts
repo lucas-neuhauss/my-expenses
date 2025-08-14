2faddd88ec21b72d55eeff89739e29c3f49778a3
Lucas Neuhaus <lucas.neuhaus93@gmail.com>
2025-08-14 10:41:14 -0300

    Fix formatting issues

--- apps/svelte/vite.config.ts
+++ apps/svelte/vite.config.ts
@@ -1,12 +1,12 @@
 import { enhancedImages } from "@sveltejs/enhanced-img";
 import { sveltekit } from "@sveltejs/kit/vite";
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vitest/config";
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [tailwindcss(), enhancedImages(), sveltekit()],
+	plugins: [tailwindcss(), enhancedImages(), sveltekit()],
 
-  test: {
-    include: ["src/**/*.{test,spec}.{js,ts}"],
-  },
+	test: {
+		include: ["src/**/*.{test,spec}.{js,ts}"],
+	},
 });
