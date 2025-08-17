3447c44fbd7771c5ea263d49276a6f8a5ad7cf88
Lucas Neuhaus <lucas.neuhaus93@gmail.com>
2025-08-17 14:31:27 -0300

    Add rollup-plugin-visualizer vite plugin

--- apps/svelte/vite.config.ts
+++ apps/svelte/vite.config.ts
@@ -1,10 +1,16 @@
 import { enhancedImages } from "@sveltejs/enhanced-img";
 import { sveltekit } from "@sveltejs/kit/vite";
 import tailwindcss from "@tailwindcss/vite";
+import { visualizer } from "rollup-plugin-visualizer";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-	plugins: [tailwindcss(), enhancedImages(), sveltekit()],
+	plugins: [
+		tailwindcss(),
+		enhancedImages(),
+		sveltekit(),
+		visualizer({ emitFile: true, filename: "stats.html" }),
+	],
 
 	test: {
 		include: ["src/**/*.{test,spec}.{js,ts}"],
